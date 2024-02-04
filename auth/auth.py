14536b27c380a9f1f5f575c8b3a2c2dff9fba623
000xzhou <xzhou92@gmail.com>
2024-02-03 21:13:29 -0500

    adding redirect

--- auth/auth.py
+++ auth/auth.py
@@ -25,7 +25,7 @@ def login():
             return render_template("auth/login.html", form=form)
         
         session['user'] = user.email
-        return "login"
+        return redirect(url_for("dashboard_bp.dashboard"))
     return render_template("auth/login.html", form=form)
 
 @auth_bp.route('/register', methods=['GET', 'POST'])
@@ -44,7 +44,6 @@ def register():
             return render_template("auth/register.html", form=form)
 
         session['user'] = new_user.email
-        return "you login"
-        # return redirect(url_for('index'))
+        return redirect(url_for("dashboard_bp.dashboard"))
 
     return render_template("auth/register.html", form=form)
--- general/general.py
+++ general/general.py
@@ -1,9 +1,11 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, session, url_for
 
 general_bp = Blueprint('general_bp', __name__,
     template_folder='templates',)
 
 @general_bp.route('/')
 def index():
+    if 'user' in session:
+        return redirect(url_for("dashboard_bp.dashboard"))
     return render_template("general/index.html")
     
