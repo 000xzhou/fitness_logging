730c2f1227549dbb0dc4752505545ba145e49f1a
000xzhou <xzhou92@gmail.com>
2024-02-05 16:32:18 -0500

    added edit schedule

--- workoutschedule/static/schedule.js
+++ workoutschedule/static/schedule.js
@@ -1,11 +1,15 @@
-const popupContainer = document.getElementById("popupContainer");
-const editBtn = document.querySelectorAll(".edit-btn");
+const schedule = document.getElementById("schedule");
 const deleteBtn = document.querySelectorAll(".delete-btn");
 const addForm = document.getElementById("add-schedule");
-const schedule = document.getElementById("schedule");
+const editBtn = document.querySelectorAll(".edit-btn");
+const editForm = document.getElementById("edit-schedule-form");
 
 deleteBtn.forEach((btn) => btn.addEventListener("click", deleteSchedule));
 addForm.addEventListener("submit", addSchedule);
+editBtn.forEach((btn) => btn.addEventListener("click", editScheduleForm));
+if (editForm) {
+  editForm.addEventListener("submit", editSchedule);
+}
 
 function deleteSchedule(e) {
   let parentElement = e.target.parentElement;
@@ -67,7 +71,64 @@ function addSchedule(e) {
     });
 }
 
+function editScheduleForm(e) {
+  // pop up for editting pop up or turn the post into an edit form
+  let parentElement = e.target.parentElement;
+  fetch("/schedule/edit_schedule/" + parentElement.id)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.text();
+    })
+    .then((data) => {
+      parentElement.innerHTML = data;
+      document
+        .getElementById("edit-schedule-form")
+        .addEventListener("submit", editSchedule);
+    })
+    .catch((error) => {
+      console.error("There was a problem with the fetch operation:", error);
+    });
+}
+
+function editSchedule(e) {
+  e.preventDefault();
+  let parentElement = e.target.parentElement;
+  let nameVal = document.getElementById("add-name");
+  let descriptionVal = document.getElementById("add-description");
+  console.log(nameVal);
+  console.log(nameVal.value);
+  const scheduleVal = {
+    name: nameVal.value,
+    description: descriptionVal.value,
+  };
+  fetch("/schedule/edit_schedule/" + parentElement.id, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(scheduleVal),
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.text();
+    })
+    .then((data) => {
+      console.log(data);
+      parentElement.innerHTML = data;
+      addEventtoBtns();
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+}
+
 function addEventtoBtns() {
   const deleteBtns = document.querySelectorAll(".delete-btn");
   deleteBtns.forEach((btn) => btn.addEventListener("click", deleteSchedule));
+  const editBtns = document.querySelectorAll(".edit-btn");
+  editBtns.forEach((btn) => btn.addEventListener("click", editScheduleForm));
 }
--- workoutschedule/workoutschedule.py
+++ workoutschedule/workoutschedule.py
@@ -1,5 +1,4 @@
 from flask import Blueprint, render_template, request, session, redirect, url_for,jsonify
-import json
 from models import WorkoutPlan, db
 
 workout_schedule_bp = Blueprint('workout_schedule_bp', __name__,
@@ -10,33 +9,52 @@ def user_calendar():
     # show full calendar with the workouts and abilty to edit
     return render_template("calendar.html")
 
+# =========================== START add edit delete schedule START ============================================
 @workout_schedule_bp.route('/', methods=['GET', 'POST'])
 def user_schedule():
-    # show full schedule with the workouts and abilty to edit
+    """ Gives user a list of their schedule."""
+    if 'user' not in session:
+        return redirect(url_for('auth_bp.login'))
     schedules = WorkoutPlan.query.all()
 
     return render_template("schedule.html", schedules=schedules)
 
 @workout_schedule_bp.route('/add_schedule', methods=['POST'])
 def add_schedule():
+    """ Let user add a schedule."""
     if 'user' not in session:
         return redirect(url_for('auth_bp.login'))
     
-    data = json.loads(request.data)
-    name = data.get('name')
-    description = data.get('description')
+    name = request.json["name"]
+    description = request.json['description']
     new_plan = WorkoutPlan(name=name, description=description, user_id = session['user'])
     db.session.add(new_plan)
     db.session.commit() 
     return render_template("workoutplan/add_schedule.html", schedule=new_plan)
+
+@workout_schedule_bp.route('/edit_schedule/<id>', methods=['GET', 'PATCH'])
+def edit_schedule(id):
+    """ Let user edit a schedule."""
+    if 'user' not in session:
+        return redirect(url_for('auth_bp.login'))
+    
+    schedule = WorkoutPlan.query.get_or_404(id)    
+        
+    if request.method == 'GET':
+        return render_template("workoutplan/edit_schedule_form.html", schedule=schedule)
+ 
+    schedule.name = request.json["name"]
+    schedule.description = request.json['description']
+    db.session.commit() 
+    return render_template("workoutplan/add_schedule.html", schedule=schedule)
+# =========================== END add edit delete schedule END ============================================
 
 @workout_schedule_bp.route('/delete_schedule', methods=['POST'])
 def delete_schedule():
     if 'user' not in session:
         return redirect(url_for('auth_bp.login'))
     
-    data = json.loads(request.data)
-    id = data.get('id')
+    id = request.json["id"]
     plan = WorkoutPlan.query.get_or_404(id)
     db.session.delete(plan)
     db.session.commit()
