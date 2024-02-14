019942ae6ccfd6aebf60dbee14102b756b4c9462
000xzhou <xzhou92@gmail.com>
2024-02-14 15:09:32 -0500

    added settings commit

--- general/general.py
+++ general/general.py
@@ -1,5 +1,5 @@
-from flask import Blueprint, render_template, redirect, session, url_for
-from models import User
+from flask import Blueprint, render_template, redirect, session, url_for, request
+from models import User, db
 from general.forms import UserForm
 
 
@@ -12,12 +12,19 @@ def index():
         return redirect(url_for("dashboard_bp.dashboard", user_email=session['user']))
     return render_template("general/index.html")
     
-@general_bp.route('/settings')
+@general_bp.route('/settings', methods=['GET', 'POST'])
 def settings():
     if 'user' not in session:
         return redirect("/")
+
     user = User.query.get_or_404(session['user'])
     form = UserForm(obj = user)
+    
     if form.validate_on_submit():
-        return render_template("general/settings.html")
+        user.email = form.email.data
+        user.reminder = form.reminder.data
+        user.weightunit = form.weightunit.data
+        db.session.commit()
+        return render_template("general/settings.html", form=form)
+    
     return render_template("general/settings.html", form=form)
