e4d967b25f4db9625216159b854267a60f9adf0c
000xzhou <xzhou92@gmail.com>
2024-02-13 19:24:37 -0500

    fix weight unit.

--- loggingworkout/loggingworkout.py
+++ loggingworkout/loggingworkout.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, session
 from models import WorkoutPlan
 
 logging_workout_bp = Blueprint('logging_workout_bp', __name__,
