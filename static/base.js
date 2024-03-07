07eb33c6d38f0058aee14991697e291dca10e931
000xzhou <xzhou92@gmail.com>
2024-03-07 15:45:26 -0500

    nav bar coloring

--- static/base.js
+++ static/base.js
@@ -1,18 +1,14 @@
 const myNavLinks = document.getElementById("myNavLinks");
 const navIconBtn = document.querySelector(".navIconBtn");
 const navIconBtnClose = document.querySelector(".navIconBtnClose");
+const menu = document.querySelector(".material-symbols-outlined");
 
 navIconBtn.addEventListener("click", () => {
   if (myNavLinks.style.display === "block") {
     myNavLinks.style.display = "none";
+    menu.textContent = "menu";
   } else {
     myNavLinks.style.display = "block";
-  }
-});
-navIconBtnClose.addEventListener("click", () => {
-  if (myNavLinks.style.display === "block") {
-    myNavLinks.style.display = "none";
-  } else {
-    myNavLinks.style.display = "block";
+    menu.textContent = "close";
   }
 });
